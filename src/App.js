a1997b41c4d68fc0ef1c4bafc7c397598f93f995
GuidoOffermans <guidooffermans@hotmail.com>
2019-09-16 17:45:09 +0200

    made a gameList, gameForm and basic styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-import request from 'superagent'
+import { connect } from 'react-redux';
 import { Route } from 'react-router-dom';
+import { url } from './constants';
 
-import Lobby from './components/Lobby'
+import { setGames } from './redux/actions';
+import Lobby from './components/Lobby';
 
 function App(props) {
 
-  useEffect(()=>{
-    
-  },[])
+  const { setGames } = props
+
+	useEffect(() => {
+		const source = new EventSource(`${url}/stream`);
+
+		source.onmessage = (event) => {
+			const { data } = event;
+
+			const games = JSON.parse(data);
+
+			setGames(games);
+		};
+	}, [setGames]);
 
 	return (
 		<div className="App">
+    <header>
+      <h1>palace</h1>
+    </header>
 			<main>
 				<Route path="/" exact component={Lobby} />
 			</main>
+    <footer>
+      <p>footer</p>
+    </footer>
 		</div>
 	);
 }
 
-export default App;
+const mapDispatchToProps = {
+	setGames
+};
+
+export default connect(null, mapDispatchToProps)(App);
--- src/components/Lobby/index.js
+++ src/components/Lobby/index.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
+
+import CreateGameForm from '../CreateGameForm/index';
+import Game from '../Game';
+import './lobby.css'
 
 const Lobby = (props) => {
-  
-  return 'lobby'
+	console.log('lobby', props.games);
+
+	const { games } = props;
+	const gameList =
+		games &&
+		games.map((game) => {
+			return <Game name={game.name} key={game.id} />;
+		});
+
+	return (
+		<div>
+			<CreateGameForm />
+			<div className='gameList'>{gameList}</div>
+		</div>
+	);
+};
+
+function mapStateToProps(state) {
+	return { games: state.games };
 }
 
-export default Lobby
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Lobby);
