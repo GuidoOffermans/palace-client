34622a3b5e1adbd9364755c2584769f92ef738c1
GuidoOffermans <guidooffermans@hotmail.com>
2019-09-16 16:31:39 +0200

    made dev branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
+import request from 'superagent'
 import { Route } from 'react-router-dom';
 
 import Lobby from './components/Lobby'
 
 function App(props) {
+
+  useEffect(()=>{
+    
+  },[])
+
 	return (
 		<div className="App">
 			<main>
