a2222d1da797f8432b5a2df50c5963d17da15dad
GuidoOffermans <guidooffermans@hotmail.com>
2019-09-17 11:51:31 +0200

    join game button

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -3,14 +3,14 @@ import './game.css';
 import { Link } from 'react-router-dom';
 import { url } from '../../constants';
 import request from 'superagent';
+import { connect } from 'react-redux';
 
 const Game = (props) => {
-	const { name, gameId } = props;
+	const { name, gameId, jwt } = props;
 
 	const addUserToGame = () => {
-    console.log('clicked')
-    request.put(`${url}/join/${gameId}`)
-    .then();
+		console.log('clicked');
+		request.put(`${url}/join/${gameId}`).set('Authorization',`Bearer ${jwt}`).then();
 	};
 
 	return (
@@ -23,4 +23,10 @@ const Game = (props) => {
 	);
 };
 
-export default Game;
+const mapStateToProps = (state) => {
+  return {
+    jwt: state.user.jwt
+  }
+}
+
+export default connect(mapStateToProps)(Game);
