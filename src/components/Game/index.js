8b85c843d78c7ba7a0a8e1adcf8808982f2a5082
GuidoOffermans <guidooffermans@hotmail.com>
2019-09-17 11:14:45 +0200

    more game-session

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import './game.css';
 import { Link } from 'react-router-dom';
+import { url } from '../../constants';
+import request from 'superagent';
 
 const Game = (props) => {
-  const { name, id } = props;
-  
-  const addUserToGame = () =>{
-    
-  }
+	const { name, gameId } = props;
+
+	const addUserToGame = () => {
+    console.log('clicked')
+    request.put(`${url}/join/${gameId}`)
+    .then();
+	};
 
 	return (
 		<div className="game">
 			<h2>{name}</h2>
-      <Link to={`/game/${id}`}><button>join</button></Link>
+			<Link to={`/game/${gameId}`}>
+				<button onClick={addUserToGame}>join</button>
+			</Link>
 		</div>
 	);
 };
